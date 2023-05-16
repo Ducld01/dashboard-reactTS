96413b1f371a11d17db11a161db5c21690993183
DucLd <ducld@toprate.io>
2023-05-17 02:27:19 +0700

    build project dashboard react with tailwindcss and antd

---
+++ src/ui/index.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-orange-300">Vite + React</h1>
+    </>
+  );
+}
+
+export { App };
---
+++ src/ui/layouts/MainLayout/index.tsx
@@ -0,0 +1,7 @@
+export function MainLayout() {
+  return (
+    <div>
+      <h1 className="text-orange-300">Vite + React</h1>
+    </div>
+  );
+}
